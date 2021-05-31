ba0fd43577725458cb9fd7b565b107167b66dbd4
PRANTA Dutta <prantadutta1997@gmail.com>
2021-05-31 18:46:20 +0600

    resized some buttons

--- src/pages/installments/[installment].tsx
+++ src/pages/installments/[installment].tsx
@@ -62,13 +62,13 @@ const Installment: React.FC<InstallmentProps> = ({ user, installmentId }) => {
             </p>
             <div className="flex justify-between">
               <button
-                className="px-4 py-2 rounded-lg bg-gray-200 text-primary focus:outline-none focus:ring-1 focus:ring-bg-gray-300"
+                className="px-4 py-2 rounded-lg bg-white text-primary font-bold focus:outline-none focus:ring-1 focus:ring-bg-gray-300 hover:text-white hover:bg-primaryAccent transition-css"
                 onClick={onClose}
               >
                 No
               </button>
               <button
-                className="px-4 py-2 rounded-lg bg-gray-200 text-primary focus:outline-none focus:ring-1 focus:ring-bg-gray-300"
+                className="px-4 py-2 rounded-lg bg-white text-primary font-bold focus:outline-none focus:ring-1 focus:ring-bg-gray-300 hover:text-white hover:bg-primaryAccent transition-css"
                 onClick={async () => {
                   await handlePay()
                   onClose()
